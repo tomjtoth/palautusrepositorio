3b73c997073853a2893eef273b0dbd682da13869
Tamás Tóth <tomjtoth@gmail.com>
2023-11-06 00:56:06 +0200

    task 11 done

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,34 @@
+from textwrap import dedent
+import requests
 from player import Player
 
+
+def filter_by_nationality(players: list, nat: str):
+
+    print(
+        f"Players from {nat}\n"
+
+        # only python 3.12 allows that linefeed within the f-string, I <3 ~~Python~~ Rust, JS, HTML, CSS, SQL
+        + "\n".join([
+
+            str(player) for player in
+
+            filter(
+                lambda elem: elem.nationality == nat,
+                players
+            )
+        ])
+    )
+
+
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    players = [Player(pla) for pla in response]
+
+    filter_by_nationality(players, "FIN")
+
 
 if __name__ == "__main__":
     main()

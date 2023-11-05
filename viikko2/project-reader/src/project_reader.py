ac227e59bf3fd4017499b26573b472f0a23e7ad2
Tamás Tóth <tomjtoth@gmail.com>
2023-11-05 19:05:38 +0200

    name, desc, deps, dev_deps extracted from toml

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+from toml import loads, dumps
 
 
 class ProjectReader:
@@ -9,7 +10,19 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        obj = loads(content)["tool"]["poetry"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+
+        return Project(
+            obj["name"],
+            obj["description"],
+            [
+                dep[0] for dep in
+                obj["dependencies"].items()
+            ],
+            [
+                dep[0] for dep in
+                obj["group"]["dev"]["dependencies"].items()
+            ]
+        )

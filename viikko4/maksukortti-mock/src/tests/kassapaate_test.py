6bacccbe405eaf8d4c612c65081bf1f03930b79b
Tamás Tóth <tomjtoth@gmail.com>
2023-11-10 22:11:24 +0200

    mocked everything (too), tests pass

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -1,15 +1,25 @@
 import unittest
-from unittest.mock import Mock, ANY
+from unittest.mock import Mock, patch
 from kassapaate import Kassapaate, HINTA
 from maksukortti import Maksukortti
+
+""" # someone tell me how to override methods for these tests
+
+@patch.object(Kassapaate, 'osta_lounas')
+def minun_osta_lounas(self, kortti: Maksukortti):
+    if kortti.saldo >= HINTA:
+        kortti.osta(HINTA)
+        self.myytyja_lounaita += 1
+
+"""
 
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
-        self.kassa = Kassapaate()
+        self.kassa = Mock(wraps=Kassapaate())
 
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
-        maksukortti_mock = Mock(wraps=Maksukortti)
+        maksukortti_mock = Mock(wraps=Maksukortti(10))
         maksukortti_mock.saldo = 10
 
         self.kassa.osta_lounas(maksukortti_mock)
@@ -17,7 +27,7 @@ class TestKassapaate(unittest.TestCase):
         maksukortti_mock.osta.assert_called_with(HINTA)
 
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
-        maksukortti_mock = Mock(wraps=Maksukortti)
+        maksukortti_mock = Mock(wraps=Maksukortti(4))
         maksukortti_mock.saldo = 4
 
         self.kassa.osta_lounas(maksukortti_mock)

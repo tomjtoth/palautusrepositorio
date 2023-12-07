d373ed30c6ab53150c371e5ac6a385e00e6534d7
Tamás Tóth <tomjtoth@gmail.com>
2023-12-07 16:37:39 +0200

    tested `QueryBuilder.one_of()` in `index.py`

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -39,11 +39,25 @@ def main():
         )
     )
 
-    matcher = QueryBuilder() \
+    query = QueryBuilder()
+    matcher = query \
         .plays_in("NYR") \
         .has_at_least(10, "goals") \
         .has_fewer_than(20, "goals") \
         .build()
+
+    m1 = query \
+        .plays_in("PHI") \
+        .has_at_least(10, "assists") \
+        .has_fewer_than(5, "goals") \
+        .build()
+
+    m2 = query \
+        .plays_in("EDM") \
+        .has_at_least(50, "points") \
+        .build()
+
+    matcher = query.one_of(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)

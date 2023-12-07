a509237fc8f65d692bbb020a0aee0e7fa2cf877e
Tamás Tóth <tomjtoth@gmail.com>
2023-12-07 14:17:34 +0200

    using the above 3 implemented classes in `main.py`

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -13,8 +14,21 @@ def main():
         PlaysIn("PHI")
     )
 
+    matcher = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+
+    matcher = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+
     for player in stats.matches(matcher):
         print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":

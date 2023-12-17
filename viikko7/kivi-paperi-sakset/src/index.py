b65da81dce0e80c386df1e860445016863ad91c4
Tamás Tóth <tomjtoth@gmail.com>
2023-12-17 18:59:59 +0200

    `index.py` updated

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,41 +1,61 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from textwrap import dedent
+from kps import KPS
 from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
+from tekoaly import Tekoaly
+from tekoaly_parannettu import TekoalyParannettu
+from tuomari import Tuomari
+
+tekoaly = Tekoaly()
+parempi_tekoaly = TekoalyParannettu()
 
 
 def main():
+    conf = {
+        'a': (
+            KPS,
+            None,
+            "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s",
+        ),
+        'b': (
+            KPSTekoaly,
+            tekoaly,
+            "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+        ),
+        'c': (
+            KPSParempiTekoaly,
+            parempi_tekoaly,
+            "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+        )
+    }
+
     while True:
-        print("Valitse pelataanko"
-              "\n (a) Ihmistä vastaan"
-              "\n (b) Tekoälyä vastaan"
-              "\n (c) Parannettua tekoälyä vastaan"
-              "\nMuilla valinnoilla lopetetaan"
-              )
+        # resetting tuomari for each game
+        tuomari = Tuomari()
+        try:
+            answer = input(dedent(
+                """\
+                Valitse pelataanko
+                 (a) Ihmistä vastaan
+                 (b) Tekoälyä vastaan
+                 (c) Parannettua tekoälyä vastaan
+                Muilla valinnoilla lopetetaan
+                """
+            )).lower()
 
-        vastaus = input()
+            the_class, the_opponent, the_text = conf[answer[0]]
 
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+            print(the_text)
 
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+            the_class(tuomari, the_opponent).pelaa()
 
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+        except (
+            # other than a,b,c
+            KeyError,
 
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
-        else:
+            # 0 long string
+            IndexError
+        ):
             break
 
 
